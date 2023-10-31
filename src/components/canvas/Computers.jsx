c9a09e977ea471332bb7a6109534a6a7df08f097
Sagar Dubey <sagardubey47@gmail.com>
2023-10-31 11:34:00 +0530

    primitive commentted

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -20,12 +20,12 @@ const Computers = ({ isMobile }) => {
         shadow-mapSize={1024}
       />
       <pointLight intensity={1} />
-      <primitive
+      {/* <primitive
         object={computer.scene}
         scale={isMobile ? 0.5 : 0.75}
         position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
-      />
+      /> */}
     </mesh>
   );
 };
